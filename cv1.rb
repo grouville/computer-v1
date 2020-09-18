102413bff107d034ea094b2ad5a44398b767cdc6
home <guillaume.derouville@gmail.com>
2020-09-18 14:34:39 +0200

    cleaner code -> use loop

--- cv1.rb
+++ cv1.rb
@@ -6,7 +6,7 @@
 def lexer(str)
 
     splitted_equation = str.gsub(" - ", "|-").gsub(" + ", " | ").gsub(" ", "").upcase().split("=")
-    raise("More than 2 parts in the equation") if splitted_equation.length() != 2
+    raise("2 parts expected in the equation") if splitted_equation.length() != 2
     normalized_equation = splitted_equation.map { |el| el.split("|") }
                                    .map { |el| el.filter { |e| !e.empty? }}
                                    .map { |el| el.map {|e| e.gsub("--", "") }}
@@ -20,24 +20,22 @@ def parser(elements)
 
     # Array that will contain the 2d arrays of parsed data
     array_hash_2d = elements.map { |el|
-        
         tmp_hash = {}
-        el.each { |e|
+            el.each { |e|
 
-            # Check that the letter X is present
-            raise("Put an X variable my dear") unless e.match?(/\*X\^/)
-            # Check that the multiplier is an int (or a float)
-            raise("Put a real int please") if e.match(/^((?:|[+-])(\d+(\.\d+)?))\*/).nil?
-            # Check that the exponent is an int
-            raise("Put a valid exponent please") if e.match(/\^((?:|[+-])\d*)$/).nil?
+                # Check that the letter X is present
+                raise("Put an X variable my dear") unless e.match?(/\*X\^/)
+                # Check that the multiplier is an int (or a float)
+                raise("Put a real int please") if e.match(/^((?:|[+-])(\d+(\.\d+)?))\*/).nil?
+                # Check that the exponent is an int
+                raise("Put a valid exponent please") if e.match(/\^((?:|[+-])\d*)$/).nil?
 
-            # Create hash + collect exponent and slope coefficient
-            exponent = e.split("^")[1].to_i
-            slope_coeff = e.split("*")[0].to_f
-            tmp_hash["x_#{exponent}"] = slope_coeff
+                # Create hash + collect exponent and slope coefficient
+                exponent = e.split("^")[1].to_i
+                slope_coeff = e.split("*")[0].to_f
+                tmp_hash["x_#{exponent}"] = slope_coeff
 
-        }
-
+            }
         tmp_hash
     }
 
@@ -48,25 +46,49 @@ def parser(elements)
     # Substract values left of equation to datastruct
     array_hash_2d[1].each { |key, value| minimized_equation[key] -= value }
     # Remove keys that compensated
-    minimized_equation = minimized_equation.filter {|key, value| value != 0 }
+    minimized_equation = minimized_equation.filter { |key, value| value != 0 }
     # Check that the remaining keys are valid
     minimized_equation.each { |key, value|
         raise("Put a valid exponent please") if key != "x_0" && key != "x_1" && key != "x_2" 
     }
     
     return minimized_equation
+
+end
+
+def print_minimized(datastruct)
+
+    l = -> (number) { a = if number >= 0 then" + " else " - " end ; return a }
+    c = -> (number) { a = if number < 0 then number * -1 else number end ; return a }
+
+    str = "The reduced form is : "
+
+    str += "#{datastruct["x_2"]}x²" unless datastruct["x_2"].nil?
+    
+    
+    ["x_1","x_0"].each | el | do
+        unless datastruct[el].nil? then
+            str += l.call(datastruct[el])
+            str += "#{c.call(datastruct[el])}"
+            str += "x" if el == "x_1"
+        end
+    end
+
+    str += " = 0"
+
+    puts str
 end
 
 def execute(str)
     
     normalized_equation = lexer(str)
     datastruct = parser(normalized_equation)
-    
+    print_minimized(datastruct)
     print datastruct
-    
+
 end
 
-def main()
+def main
 
     print("Enter your input: ")
     str = gets.chomp
